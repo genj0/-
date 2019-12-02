62645a616e6d3a556663385b8c130e7dbdfcccba
Genjo <gena.1088@bk.ru>
2019-12-02 17:32:32 +0300

    almost done

---
+++ send.php
@@ -0,0 +1,24 @@
+<?php
+$fio = $_POST['fio'];
+$email = $_POST['email'];
+$phone = $_POST['phone'];
+$fio = htmlspecialchars($fio);
+$email = htmlspecialchars($email);
+$phone = htmlspecialchars($phone);
+$fio = urldecode($fio);
+$email = urldecode($email);
+$phone = urldecode($phone);
+$fio = trim($fio);
+$email = trim($email);
+$phone = trim($phone);
+//echo $fio;
+//echo "<br>";
+//echo $email;
+//echo "<br>;
+//echo $phone;
+if (mail("info@mmoctex.ru", "Заявка с сайта", "ФИО:".$fio.". E-mail: ".$email.".phone: ".$phone ,"From: info@moctex.ru \r\n"))
+ {     echo "сообщение успешно отправлено";
+} else {
+    echo "при отправке сообщения возникли ошибки";
+}
+?>
